87ee1f2c7fc1846d2605fe2bc4f7fc47f7d8261f
Pranav Nutalapati <pranavnutalapati@gmail.com>
2020-06-28 15:46:14 +0530

    Updated name and description

--- build.py
+++ build.py
@@ -1,8 +1,8 @@
 from cx_Freeze import setup, Executable
 
-setup(name="league-vcs",
+setup(name="League VCS",
       version="0.1.0",
-      description="",
+      description="Version control system and replay watcher for League of Legends.",
       executables=[
           Executable("entrypoints/main.py", targetName='League VCS',
                      icon='raster/icon.ico', base='Win32GUI'),
